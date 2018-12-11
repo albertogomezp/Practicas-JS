a9de36bf9cb7721207b31b9356903ce9d438f976
Alberto Gómez Peña <albertogomp@gmail.com>
2018-12-11 17:44:16 +0100

    Create 01-hola-mundo.js

---
+++ 01-hola-mundo.js
@@ -0,0 +1,5 @@
+		alert("Hola Mundo con JS"); //Esto son cuadros de diálogo
+		alert("Bienvenido al curso"); //primera parte
+		//No me es necesario añadir los comentarios multilineas y normales, thanks java :3
+		console.log("Muestra esto en la consola"); //segunda parte
+		//console.log(2*2); Todos los comentarios me son indiferentes, nada nuevo 
