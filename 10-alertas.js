470f43e670ff55015292c7b529e8a0edec859987
Alberto Gómez Peña <albertogomp@gmail.com>
2018-12-11 17:46:03 +0100

    Create 10-alertas.js

---
+++ 10-alertas.js
@@ -0,0 +1,11 @@
+'use strict'
+alert("Esta es mi alerta");
+
+//CONFIRMACION
+var result=confirm("ESTAS SEGURO?");
+confirm("100%????");
+console.log(result);  
+
+//INGRESO DATOS
+var result2=prompt("¿QUE EDAD TIENES?",18);
+console.log(result2);
