f3fdd264850fd30c5ddefb5c65218b14ee670d11
Alberto Gómez Peña <albertogomp@gmail.com>
2018-12-11 17:44:45 +0100

    Create 03-let_y_var.js

---
+++ 03-let_y_var.js
@@ -0,0 +1,18 @@
+'use strict'
+alert("HOLA     ('0')/");
+var numero=40;
+console.log(numero); 
+if(true)
+{
+	var numero=50;
+	console.log(numero);
+}
+console.log(numero);
+let texto="Text";
+console.log(texto);
+if(true)
+{
+	let texto="Curso ";
+	console.log(texto);
+}
+console.log(texto);
