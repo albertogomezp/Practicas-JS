ea686b33f699c010c772828e257c181c3e473fc4
Alberto Gómez Peña <albertogomp@gmail.com>
2018-12-11 17:45:04 +0100

    Create 06-condicionales.js

---
+++ 06-condicionales.js
@@ -0,0 +1,30 @@
+'use strict'
+//Condicional IF
+var edad=17;
+var edad1=3;
+var edad2=12;
+var nombre="Alberto Gomez";
+if(edad1<edad2) //si pasa esto
+{ //ejecuta esto
+console.log("Edad1 es mayor que Edad 2");
+}
+else
+{
+console.log("La Edad1 es inferior");	
+}
+if(edad >=18)
+{
+console.log(nombre+' '+"tiene "+edad+" Es mayor de edad");	
+}
+else
+{
+console.log(nombre+" es menor de edad");	
+}
+if(edad<=18 && edad>=16)
+{
+console.log("Esta persona está en bachillerato");	
+}
+else
+{
+console.log("esta persona NO está en bachillerato");	
+}
