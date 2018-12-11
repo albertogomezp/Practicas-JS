de4f3cf34e59eb85ff7050129405e25d108f4a96
Alberto Gómez Peña <albertogomp@gmail.com>
2018-12-11 17:45:01 +0100

    Create 05-tipos_de_datos.js

---
+++ 05-tipos_de_datos.js
@@ -0,0 +1,16 @@
+'use strict'
+//operadores
+var num1,num2,num3; //mejor declararlo en una linea
+num1=7;
+num2=8;
+num3=num1+num2;
+console.log("LA SUMA DE num1 y num2 ES "+num3); 
+alert(num3);
+var numint=44;
+var cadena="text";
+//podemos usar comillas dobles y simples, las dobles son mas importantes que las simples.
+var verdadero= true; /
+console.log(verdadero);
+var numerofalso="33";
+console.log(Number(numerofalso)+2); //Es una string pero con number lo cambia, parseint y parsefloat idem
+console.log(typeof verdadero); //imprime de que es, esto puede sernos util mas adelante 
