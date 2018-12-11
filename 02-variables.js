d68443ab2859128a928fc2c3669621422b3a3cee
Alberto Gómez Peña <albertogomp@gmail.com>
2018-12-11 17:44:21 +0100

    Create 02-variables.js

---
+++ 02-variables.js
@@ -0,0 +1,15 @@
+'use strict'
+
+var pais="España" 
+var continente="EU WEST"
+var antiguedad =2019;
+console.log("VIVA "+pais,continente,antiguedad);
+var pais_y_continente =pais+' '+continente;
+console.log(pais_y_continente);
+pais="ZPANIA";
+continente="EHUH GUEST"
+console.log("BIBA "+pais);
+pais_y_continente =pais+' '+continente;
+console.log(pais_y_continente);
+let prueba ="hola"; 
+alert(prueba); 
